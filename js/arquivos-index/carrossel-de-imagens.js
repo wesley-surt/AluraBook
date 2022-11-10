b3325d1b215a3e77b0cc23e119a4d8ac5f748b1e
Wesley Bruno de Souza <wesleybruno.adm@gmail.com>
2022-11-10 02:45:28 -0300

    Terminando seção carrossel de imagens

--- js/arquivos-index/carrossel-de-imagens.js
+++ js/arquivos-index/carrossel-de-imagens.js
@@ -1,18 +1,21 @@
-import { criaConjuntoDeBolinhas } from "../modulos-e-funcoes/funcoes/cria-quantdd-necessaria-de-bolinhas.js";
+import { criaBolinhas } from "../modulos-e-funcoes/funcoes/cria-quantdd-necessaria-de-bolinhas.js";
 import { trocaDeImagem } from "../modulos-e-funcoes/funcoes/troca-de-imagem.js";
 
 var botaoDeProximo = document.getElementById('proximo');
 var botaoDeVoltar = document.getElementById('voltar');
 var imagemAtual = document.getElementById('image--atual');
 var imagens = document.querySelectorAll('.image');
-var conjuntoDeBolinhs = document.querySelector('.conjunto-de-bolinhas');
+var bolinhas = document.querySelector('.bolinhas');
 var idDaBolinhaDaImagemAtual = 0;
 
-criaConjuntoDeBolinhas(imagens, conjuntoDeBolinhs);
+criaBolinhas(imagens, bolinhas);
 
-//onmousedown = function() {
-//    slide.style.paddingLeft = 0 + 'px';
-
+imagens.forEach(imagem => {
+    imagem.onmousedown = function() {
+        imagemAtual.style.marginLeft = 0 + 'px';
+    };
+    
+})
 
 botaoDeProximo.addEventListener('click', () => {
     idDaBolinhaDaImagemAtual++;
--- js/modulos-e-funcoes/funcoes/cria-quantdd-necessaria-de-bolinhas.js
+++ js/modulos-e-funcoes/funcoes/cria-quantdd-necessaria-de-bolinhas.js
@@ -1,9 +1,9 @@
-export var criaConjuntoDeBolinhas = (imagens, conjuntoDeBolinhs) => {
+export var criaBolinhas = (imagens, bolinhas) => {
     for (let i = 0; i < imagens.length; i++) {
         var divBolinha = document.createElement('div');
         var idDaBolinha = i;
         
         divBolinha.id = idDaBolinha;
-        conjuntoDeBolinhs.append(divBolinha);
+        bolinhas.append(divBolinha);
     }
 }
